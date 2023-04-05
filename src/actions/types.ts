e824058eb391c1a6f9080ff7491ffcdc7c55836b
andrei <ganeevandrej21@gmail.com>
2023-04-05 21:28:57 +0300

    Типизация reducer и actionCreators.

---
+++ src/actions/types.ts
@@ -0,0 +1,37 @@
+import {
+    ALL_BOOKS_REMOVED_FROM_CART,
+    BOOK_ADDED_TO_CART,
+    BOOK_REMOVED_FROM_CART,
+    FETCH_BOOKS_FAILURE,
+    FETCH_BOOKS_REQUEST,
+    FETCH_BOOKS_SUCCESS
+} from "../action-types";
+import { BookType } from "../reducers";
+
+
+export type booksLoadedAction = {
+    type: typeof FETCH_BOOKS_SUCCESS,
+    payload: BookType[]
+}
+export type booksRequestedAction = {
+    type: typeof FETCH_BOOKS_REQUEST
+}
+export type booksErrorAction = {
+    type: typeof FETCH_BOOKS_FAILURE
+}
+export type bookAddedToCartAction = {
+    type: typeof BOOK_ADDED_TO_CART,
+    payload: number
+}
+export type bookRemovedFromCartAction = {
+    type: typeof BOOK_REMOVED_FROM_CART,
+    payload: number
+}
+export type allBooksRemovedFromCartAction = {
+    type: typeof ALL_BOOKS_REMOVED_FROM_CART,
+    payload: number
+}
+
+export type bookListTypes = booksLoadedAction | booksRequestedAction | booksErrorAction;
+export type shoppingCartTypes = bookAddedToCartAction | bookRemovedFromCartAction | allBooksRemovedFromCartAction;
+
